ff779546611aefe73413b31d65b0e8f683f1f9d8
Makeda Lelozwi Dumeko <lelodumeko2@gmail.com>
2024-06-09 17:37:02 +0200

    started with routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
-import { useState } from 'react'
-
+import React from "react";
+import ReactDom from "react-dom/client";
 
 
 function App() {
--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,19 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { BrowserRouter, Routes, Route, Link } from "react-router-dom";
+import App from "./App.jsx";
+import "./index.css";
 
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <BrowserRouter>
+    <nav>
+      <Link to="/">Home</Link>
+      <Link to="/about">About</Link>
+    </nav>
+
+    <Routes>
+      <Route path="/" element={<App />} />
+      <Route path="/about" element={<div>About</div>} />
+    </Routes>
+  </BrowserRouter>
+);
