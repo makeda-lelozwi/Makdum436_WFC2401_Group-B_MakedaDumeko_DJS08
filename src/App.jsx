0d96cc458bbe42f51f41aeaceb22d8a6aa8c5544
Makeda Lelozwi Dumeko <lelodumeko2@gmail.com>
2024-06-09 21:46:57 +0200

    completed project

--- src/App.jsx
+++ src/App.jsx
@@ -10,6 +10,13 @@ import AuthRequired from "./components/AuthRequired.jsx";
 import Login from "./pages/Login.jsx";
 import Dashboard from "./pages/Host/Dashboard.jsx";
 import NotFound from "./pages/NotFound.jsx";
+import HostVanDetail from "./pages/Host/HostVanDetail.jsx";
+import Income from "./pages/Host/Income.jsx";
+import Reviews from "./pages/Host/Reviews.jsx";
+import HostVans from "./pages/Host/HostVans.jsx";
+import HostVanInfo from "./pages/Host/HostVanInfo.jsx";
+import HostVanPricing from "./pages/Host/HostVanPricing.jsx";
+import HostVanPhotos from "./pages/Host/HostVanPhotos.jsx";
 
 import "../src/server.js";
 
@@ -27,6 +34,14 @@ function App() {
           <Route element={<AuthRequired />}>
             <Route path="host" element={<HostLayout />}>
               <Route index element={<Dashboard />} />
+              <Route path="income" element={<Income />} />
+              <Route path="reviews" element={<Reviews />} />
+              <Route path="vans" element={<HostVans />} />
+              <Route path="vans/:id" element={<HostVanDetail />}>
+                <Route index element={<HostVanInfo />} />
+                <Route path="pricing" element={<HostVanPricing />} />
+                <Route path="photos" element={<HostVanPhotos />} />
+              </Route>
             </Route>
           </Route>
           <Route path="*" element={<NotFound />} />
